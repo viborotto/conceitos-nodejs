98747a26a68eb3b3473d70589402cc9162c86a9d
viborotto <vittoriaborotto@gmail.com>
2021-08-26 12:31:19 -0300

    Desafio Realizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,62 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+    const repository = { id: uuid(), title: 'Desafio Node.js', url: 'http://github.com/viborotto/conceitos-nodejs', techs: ["Node.js", "JavaScript"], likes: 0 };
+
+    repositories.push(repository);
+    return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body;
+    const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if (repositoryIndex < 0) {
+        return response.status(400).json({
+            error: "repository not found"
+        });
+    }
+
+    const repository = {
+        id,
+        title,
+        url,
+        techs
+    };
+    repositories[repositoryIndex] = repository;
+    return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    // TODO
+    const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({
+            error: "repository not found"
+        });
+    }
+
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    // TODO
+    //aumentar o numero de likes do repository specifico
+    const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    repositories[repositoryIndex].likes += 1;
+
+    return response.status(201).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
